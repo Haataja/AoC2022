85247dec544a474aa7e767b83763625ca30113e4
Haataja <hanna.haataja@tuni.fi>
2022-12-01 16:41:21 +0200

    Add better solution for day01 and init day02

--- src/main/kotlin/day01.kt
+++ src/main/kotlin/day01.kt
@@ -1,6 +1,6 @@
 import java.io.File
 
-fun main(args: Array<String>) {
+fun main2(args: Array<String>) {
     var currentMax = 0
     val array = mutableListOf<Int>()
     var sum = 0
@@ -10,7 +10,7 @@ fun main(args: Array<String>) {
             println("Plank! CM $currentMax and SUM $sum")
             array.add(sum)
             // for the first part of just finding the top elf
-            currentMax = if(currentMax > sum) currentMax else sum
+            currentMax = if (currentMax > sum) currentMax else sum
             sum = 0
         } else {
             sum += it.toInt()
@@ -20,4 +20,20 @@ fun main(args: Array<String>) {
     array.sortDescending()
     println("current max: $currentMax")
     println("top 3 elves: ${array[0]} + ${array[1]} + ${array[2]} = ${array[0] + array[1] + array[2]}")
+}
+
+// after looking at better answers, I came up with more elegant solution
+fun main(args: Array<String>) {
+
+    val max = File("src/main/input/day01.txt").readText().split(Regex("(\\r?\\n){2,}")).maxOfOrNull { it ->
+        it.lines().sumOf { it.toInt() }
+    }
+
+    println("Elf that has most of calories has $max calories.")
+
+    val sumOfThree = File("src/main/input/day01.txt").readText().split(Regex("(\\r?\\n){2,}")).map { it ->
+        it.lines().sumOf { it.toInt() }
+    }.sortedDescending().subList(0, 3).sum()
+    println("The 3 Elves that have most of calories have $sumOfThree calories in total.")
+
 }
---
+++ src/main/kotlin/day02.kt
@@ -0,0 +1,8 @@
+import java.io.File
+
+class day02 {
+
+    fun main(args: Array<String>) {
+        File("src/main/input/day02.txt").readText()
+    }
+}
