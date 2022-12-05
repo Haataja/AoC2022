8579495f4c59d3bf9e89ab8e895f5dd7c1b79e28
Haataja <hanna.haataja@tuni.fi>
2022-12-05 11:12:42 +0200

    Add solution for day05 and init day06

--- src/main/kotlin/day05.kt
+++ src/main/kotlin/day05.kt
@@ -1,9 +1,53 @@
 import java.io.File
 
+// Well this is huge pile of crap,  maybe I refine this later on
 fun main() {
+    val start = mutableListOf<MutableList<String>>(
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf(),
+        mutableListOf()
+    )
 
-    val value = File("src/main/input/day05.txt").readText()
+    val value = File("src/main/input/day05.txt").readText().split("\n")
 
-    println("$value")
+    value.forEachIndexed { index, s ->
+        if (index < 8) { // 8
+            val columns = s.chunked(4)
+            columns.forEachIndexed { ki, k ->
+                val crate = k.replace("[", "").replace("]", "").trim()
+                if (crate.isNotBlank()) start[ki].add(crate)
+            }
+        }
+    }
+    // println("Staring from $start")
+
+    value.forEachIndexed { index, s ->
+        if (index > 9) { // 8
+            println(s.substring(14..17))
+            val numberOfCrates = s.substring(0..7).filter { c -> c.isDigit() }.toInt() - 1
+            val from = s.substring(10..14).filter { c -> c.isDigit() }.toInt() - 1
+            val to = s.substring(14 until s.length).filter { c -> c.isDigit() }.toInt() - 1
+            println("move $numberOfCrates from $from to $to")
+            val moved = mutableListOf<String>()
+            for (i in 0..numberOfCrates) {
+                moved.add(start[from][0])
+                //first part:  start[to].add(0,start[from][0])
+                start[from].removeFirst()
+            }
+            start[to].addAll(0, moved)
+            moved.clear()
+            // println(start.toString())
+        }
+    }
+
+
+
+    start.forEach { l -> print(l[0]) }
 
 }
---
+++ src/main/kotlin/day06.kt
@@ -0,0 +1,9 @@
+import java.io.File
+
+
+fun main() {
+    val value = File("src/main/input/day06.txt").readText()
+
+    println("$value")
+
+}
