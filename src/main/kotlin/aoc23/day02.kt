727e6a40c4be4d63a61043656019669f4be4dc3f
Haataja <hanna.haataja@tuni.fi>
2023-12-02 11:22:12 +0200

    Add day 2

--- src/main/kotlin/aoc23/day02.kt
+++ src/main/kotlin/aoc23/day02.kt
@@ -6,13 +6,58 @@ fun main() {
 
     val path = "src/main/input/aoc23/day02.txt"
     val rows = File(path).readText().split(Regex("(\\r?\\n)+"))
+    val limit = mapOf("red" to 12, "green" to 13, "blue" to 14)
 
     fun part1(rows: List<String>): Int {
-      return 0
+        return rows.sumOf {
+            val game = it.substring(0, it.indexOf(":")).filter { c -> c.isDigit() }.toInt()
+            val rounds = it.substring(it.indexOf(":") + 1).split(";")
+
+            var isPossible = true
+            for(round in rounds){
+                val cubes = round.split(",")
+                for (cube in cubes){
+                    val amount = cube.filter { c -> c.isDigit() }.toInt()
+                    val colour = cube.filter { c -> !c.isDigit() }.trim()
+                    if(limit[colour]!! < amount){
+                        isPossible = false
+                        break
+                    }
+                }
+                // println("$round : $isPossible")
+                if(!isPossible){
+                    break
+                }
+            }
+
+            if(isPossible){
+                game
+            } else {
+                0
+            }
+        }
     }
 
     fun part2(rows: List<String>): Int {
-        return 0
+        return rows.sumOf {
+            val rounds = it.substring(it.indexOf(":") + 1).split(";")
+
+            val minCubes = mutableMapOf("red" to 0, "green" to 0, "blue" to 0)
+            for(round in rounds){
+                val cubes = round.split(",")
+                for (cube in cubes){
+                    val amount = cube.filter { c -> c.isDigit() }.toInt()
+                    val colour = cube.filter { c -> !c.isDigit() }.trim()
+                    if(minCubes[colour]!! < amount){
+                        minCubes[colour] = amount
+                    }
+                }
+            }
+
+            minCubes.values.reduce { accumulator, element ->
+                accumulator * element
+            }
+        }
     }
 
     println(part1(rows))
