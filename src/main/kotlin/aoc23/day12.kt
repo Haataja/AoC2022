946346e916e13033c0c18f6163b76d2954e68dd3
Haataja <hanna.haataja@kolumbus.fi>
2023-12-13 22:31:37 +0200

    Add day 12 part 2

--- src/main/kotlin/aoc23/day12.kt
+++ src/main/kotlin/aoc23/day12.kt
@@ -1,8 +1,6 @@
 package me.haataja.aoc23
 
 import java.io.File
-import java.time.LocalDateTime
-import java.time.LocalTime
 
 fun main() {
 
@@ -68,14 +66,84 @@ fun main() {
         }
         return true
     }
+    fun recursiveFunction(permutation: String, group: List<Int>): Long{
+        //println(permutation)
+        return if(!permutation.contains("?")){
+            if (checkMach(permutation,group)){
+                1
+            } else {
+                0
+            }
+        } else {
+            if(checkPartial(permutation, group)){
+                recursiveFunction(permutation.replaceFirst('?','.'), group) +
+                        recursiveFunction(permutation.replaceFirst('?','#'), group)
+            } else {
+                0
+            }
+        }
 
-    fun part1(rows: List<String>): Int {
+    }
+
+    // had to look this map solution from internet, because without it completion took too much time
+    val map = mutableMapOf<String,MutableMap<List<Int>, Long>>()
+    fun tryHard(permutation: String, groups: List<Int>): Long{
+        if(map.keys.contains(permutation)){
+            if(map[permutation]!!.keys.contains(groups)){
+                return map[permutation]!![groups]!!
+            }
+        }
+
+        if(permutation.isBlank()){
+            return if (groups.isEmpty()){
+                1
+            } else {
+                0
+            }
+        }
+
+        val p = if (permutation[0] == '.'){
+            tryHard(permutation.substring(1), groups)
+        } else if (permutation[0] == '?') {
+            tryHard("#${permutation.substring(1)}", groups) + tryHard(".${permutation.substring(1)}", groups)
+        } else {
+            if(groups.isNotEmpty()){
+                val group = groups[0]
+                if (group == permutation.length && permutation.all { it == '?' || it == '#' }){
+                    if (groups.size > 1){
+                        0
+                    } else {
+                        1
+                    }
+                }else if(permutation.length > group  && permutation.substring(0, group).all { it == '?' || it == '#' }){
+                    val newGroups = groups.subList(1, groups.size)
+                    if(permutation[group] == '.'){
+                        tryHard(permutation.substring(group + 1), newGroups)
+                    } else if(permutation[group] == '?'){
+                        tryHard(".${permutation.substring(group + 1)}", newGroups)
+                    } else {
+                        0
+                    }
+                } else {
+                    0
+                }
+            } else {
+                0
+            }
+        }
+        map.put(permutation, mutableMapOf(groups to p))
+        return p
+    }
+
+    fun part1(rows: List<String>): Long {
         val sum = rows.sumOf{
             val split = it.split(Regex("\\s"))
+            val groups = split[1].split(",").map { number -> number.toInt() }
+            /* First brute force
             val pre = mutableListOf<String>()
             val permutations = mutableListOf<String>()
-            val groups = split[1].split(",").map { number -> number.toInt() }
             permutations.add(split[0])
+
             while (permutations.last().contains("?")){
                 pre.addAll(permutations)
                 permutations.clear()
@@ -85,6 +153,7 @@ fun main() {
                 }
                 pre.clear()
             }
+
             //println(permutations)
             //println(groups)
             permutations.sumOf { permutation ->
@@ -95,47 +164,29 @@ fun main() {
                     0
                 }
             }
+
+             */
+            // after working on part two did the recursive way
+            recursiveFunction(split[0], groups)
         }
         return sum
     }
 
-    fun recursiveFunction(permutation: String, group: List<Int>): Long{
-        println(permutation)
-        return if(!permutation.contains("?")){
-            if (checkMach(permutation,group)){
-                1
-            } else {
-                0
-            }
-        } else {
-            if(checkPartial(permutation, group)){
-                recursiveFunction(permutation.replaceFirst('?','.'), group) +
-                    recursiveFunction(permutation.replaceFirst('?','#'), group)
-            } else {
-                0
-            }
-        }
-
-    }
-
     fun part2(rows: List<String>): Long {
-        val start = LocalTime.now().toSecondOfDay()
         val sum = rows.sumOf{
             val split = it.split(Regex("\\s"))
             val groups = split[1].split(",").map { number -> number.toInt() }
             val allGroups = mutableListOf<Int>()
             repeat(5){allGroups.addAll(groups)}
             val first = "${split[0]}?${split[0]}?${split[0]}?${split[0]}?${split[0]}"
-            val somethin = recursiveFunction(first, allGroups)
+            val somethin = tryHard(first, allGroups)
             println("$it $somethin")
             somethin
 
         }
-        val stop = LocalTime.now().toSecondOfDay()
-        println("${LocalTime.ofSecondOfDay((stop - start).toLong())}")
         return sum
     }
 
-    //println(part1(rows))
+    println(part1(rows))
     println(part2(rows))
 }
