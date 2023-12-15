40c1a1c6c089178c0318d489ef45643f22248dac
hanna.haataja <hanna.haataja@solita.fi>
2023-12-15 17:58:01 +0200

    Add day 15

--- src/main/kotlin/aoc23/day15.kt
+++ src/main/kotlin/aoc23/day15.kt
@@ -5,16 +5,53 @@ import java.io.File
 fun main() {
 
     val path = "src/main/input/aoc23/day15.txt"
-    val rows = File(path).readText().split(Regex("(\\r?\\n)+"))
+    val rows = File(path).readText().split(",")
 
-    fun part1(): Int {
-        return 0
+    fun hash(it: String): Int {
+        var value = 0
+        it.forEach { c ->
+            value = (value + c.code.toByte().toInt()) * 17 % 256
+        }
+        return value
+    }
+
+    fun part1(rows: List<String>): Int {
+        return rows.sumOf {
+            hash(it)
+        }
     }
 
     fun part2(rows: List<String>): Int {
-        return 0
+        val boxes = mutableMapOf<Int, MutableList<Pair<String, Int>>>()
+        rows.forEach {
+            if(it.contains("=")){
+                val split = it.split("=")
+                val box = hash(split[0])
+                if(boxes.keys.contains(box) && boxes[box]!!.any { pair -> pair.first == split[0] }){
+                    val index = boxes[box]!!.indexOfFirst { pair -> pair.first == split[0] }
+                    boxes[box]!!.removeAt(index)
+                    boxes[box]!!.add(index, Pair(split[0], split[1].toInt()))
+                } else {
+                    if (boxes[box].isNullOrEmpty()){
+                        boxes[box] = mutableListOf(Pair(split[0], split[1].toInt()))
+                    } else {
+                        boxes[box]!!.add(Pair(split[0], split[1].toInt()))
+                    }
+                }
+            } else {
+                val label = it.substring(0 until it.lastIndex)
+                val box = hash(label)
+                if(!boxes[box].isNullOrEmpty() && boxes[box]!!.any { pair -> pair.first == label }){
+                    //println("$label $box ${boxes[box]!!.any { pair -> pair.first == label }}")
+                    boxes[box]!!.removeAt(boxes[box]!!.indexOfFirst { pair -> pair.first == label })
+                }
+            }
+        }
+        var sum = 0
+        boxes.forEach { it.value.forEachIndexed { index, pair -> sum += (it.key + 1) * (index + 1) * pair.second  }}
+        return sum
     }
 
-    println(part1())
+    println(part1(rows))
     println(part2(rows))
 }
